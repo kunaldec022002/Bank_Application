f5f115d41763f8d0b1707ce35fbc3026527cbf3e
kunaldec022002 <kunalingale72@gmail.com>
2025-03-27 13:25:57 +0530

    creating login and signup templates for user and aslo add bootstrap for responsive design

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template
+# main file of the preject
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# routing html pages
+
 @app.route('/')
-def index ():
+def index():
   return render_template("index.html")
 
-if(__name__ == "__main__"):
+@app.route('/signup')
+def signup():
+  return render_template("signup.html")
+
+if __name__ == '__main__':
   app.run(debug=True)
