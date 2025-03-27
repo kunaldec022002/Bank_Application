1313801b6ca16c78d0357ef5137c85f7cc0c98ba
kunaldec022002 <kunalingale72@gmail.com>
2025-03-27 21:25:34 +0530

    add database

---
+++ database.py
@@ -0,0 +1,21 @@
+# database class to perform CRUD operation
+import pymysql as pm
+
+class Database:
+    def __init__(self):
+        # Correct the connection attribute name to self.com
+        self.com = pm.connect(host='localhost', user='root', password='admin', database='mypythonbank')
+        self.cursor = self.com.cursor()  # Use self.com instead of self.con
+
+    def storeUser(self, email, pass1):
+        try:
+            # Using parameterized queries to avoid SQL injection
+            self.cursor.execute("INSERT INTO users (email, password) VALUES (%s, %s)", (email, pass1))
+            self.com.commit()  # Commit the transaction
+            self.status = True
+        except Exception as e:
+            # Rollback in case of error
+            self.com.rollback()
+            self.status = False
+           # print(f"Error: {e}")  # Print the error message for debugging
+        return self.status
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # main file of the preject
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from database import Database
 
 app = Flask(__name__)
+db = Database()
 
 # routing html pages
 
@@ -13,6 +15,25 @@ def index():
 @app.route('/signup')
 def signup():
   return render_template("signup.html")
+
+# action routes pages (storeUser)
+@app.route('/storeUser',methods=['POST','GET'])
+def storeUser():
+  if request.method == 'POST':
+    email1 = request.form['email1']
+    pass1 = request.form['pass1']
+    pass2 = request.form['pass2']
+
+    if pass1 != pass2:
+      msg="password not matched, please try again !!"
+    else:
+      if db.storeUser(email1,pass1) == True:
+        msg= "signup successful"
+      else:
+        msg = "Signup Failed"   
+    return render_template("message.html",msg=msg)
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
